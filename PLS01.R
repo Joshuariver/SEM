908b0575877e242ea9d8a7ad2aa7691e8db247ff
Joshua River <joshuariver@naver.com>
2019-04-21 23:08:04 +0900

    Update PLS01.R

--- PLS01.R
+++ PLS01.R
@@ -1,14 +1,18 @@
+# Source: PLS Path Modeling with R
+# by Gaston Sanchez
+# www.gastonsanchez.com
+
 rm(list=ls())
 setwd("~/R/Structural Equation Modeling")
 
 # installing colortools (this is a comment!)
-install.packages("colortools")
+# install.packages("colortools")
 
 # loading colortools (this is another comment!)
 library(colortools)
 
 # installing the package 'plspm'
-install.packages("plspm")
+# install.packages("plspm")
 
 # load package 'plspm'
 library("plspm")
@@ -78,3 +82,132 @@ head(foot_pls$scores, n = 5)
 
 # show me the last scores
 tail(foot_pls$scores, n = 5)
+
+# unidimensionality
+foot_pls$unidim
+
+# cronbach's alpha
+foot_pls$unidim[, 3, drop = FALSE]
+
+# dillon-goldstein rho
+foot_pls$unidim[, 4, drop = FALSE]
+
+# eigenvalues
+foot_pls$unidim[, 5:6]
+
+# plotting loadings
+plot(foot_pls, what = "loadings")
+
+# outer model results
+foot_pls$outer_model
+
+# Defense outer model results
+subset(foot_pls$outer_model, block == "Defense")
+
+# plotting weights
+plot(foot_pls, what = "weights")
+
+# defense loading 값이 음의 값을 보이므로 이를 양의 값으로 전환함.
+# add two more columns NGCH and NGCA
+spainfoot$NGCH = -1 * spainfoot$GCH
+spainfoot$NGCA = -1 * spainfoot$GCA
+
+# check column names
+names(spainfoot)
+
+# new list of blocks (with column positions of variables)
+new_blocks_pos = list(1:4, c(15,16,7,8), 9:12)
+
+# new list of blocks (with names of variables)
+new_blocks_str = list(
+  c("GSH", "GSA", "SSH", "SSA"),
+  c("NGCH", "NGCA", "CSH", "CSA"),
+  c("WMH", "WMA", "LWR", "LRWL"))
+
+# re-apply plspm
+foot_pls = plspm(spainfoot, foot_path, new_blocks_str, modes = foot_modes)
+# plot loadings
+plot(foot_pls, "loadings")
+
+# unidimensionality
+foot_pls$unidim
+
+# loadings and communalities
+foot_pls$outer_model
+
+# cross-loadings
+foot_pls$crossloadings
+
+# load ggplot2 and reshape
+library(ggplot2)
+library(reshape)
+
+# reshape crossloadings data.frame for ggplot
+xloads = melt(foot_pls$crossloadings, id.vars = c("name", "block"),
+              variable_name = "LV")
+
+# bar-charts of crossloadings by block
+ggplot(data = xloads,
+       aes(x = name, y = value, fill = block)) +
+  # add horizontal reference lines
+  geom_hline(yintercept = 0, color = "gray75") +
+  geom_hline(yintercept = 0.5, color = "gray70", linetype = 2) +
+  # indicate the use of car-charts
+  geom_bar(stat = 'identity', position = 'dodge') +
+  # panel display (i.e. faceting)
+  facet_wrap(block ~ LV) +
+  # tweaking some grahical elements
+  theme(axis.text.x = element_text(angle = 90),
+        line = element_blank(),
+        plot.title = element_text(size = 12)) +
+  # add title
+  ggtitle("Crossloadings")
+
+# inner model
+foot_pls$inner_model
+
+# inner model summary
+foot_pls$inner_summary
+
+# select R2
+foot_pls$inner_summary[, "R2", drop = FALSE]
+
+# R Square의 해석. (절대적인 것은 아님)
+# 1. Low: R < 0:30 (although some authors consider R < 0:20)
+# 2. Moderate: 0:30 < R < 0:60 (you can also find 0:20 < R < 0:50)
+# 3. High: R > 0:60 (alternatively there’s also R > 0:50)
+
+# inner model summary
+foot_pls$inner_summary
+
+
+# The GoF index is a pseudo Goodness of fit measure that accounts for the model quality at
+# both the measurement and the structural models. GoF is calculated as the geometric mean of
+# the average communality and the average R2 value. Since it takes in to account communality,
+# this index is more applicable to reflective indicators than to formative indicators. 
+
+# a GoF value of 0.78 could be interpreted as if the prediction power of the model is of 78%. 
+# The naive rule of thumb is: the higher, the better. Acceptable \good" values within the PLS-PM 
+# community are GoF >0.7
+
+# gof index
+foot_pls$gof
+
+
+# Bootstrapping
+# 
+# running bootstrap validation
+foot_val = plspm(spainfoot, foot_path, new_blocks_str, modes = foot_modes,
+                 boot.val = TRUE, br = 200)
+
+# bootstrap results
+foot_val$boot
+
+# What we obtain in foot val$boot is a list with results for:
+# • the outer weights (foot val$boot$weigts)
+# • the loadings (foot val$boot$loadings)
+# • the path coefficients (foot val$boot$paths)
+# • the R2 (foot val$boot$rsq)
+# • the total effects (foot val$boot$total.efs)
+
+
