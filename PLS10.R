9493f8b132d6f8d91580f21f9f9195fd4e747e26
Joshua River <joshuariver@naver.com>
2019-04-23 23:19:26 +0900

    Update PLS10.R

--- PLS10.R
+++ PLS10.R
@@ -61,3 +61,29 @@ nfl_pls2 = plspm(off_twostep, nfl_path, nfl_blocks2, modes = nfl_modes)
 
 # plot path coeffs
 plot(nfl_pls2)
+
+
+
+# some traps beware of
+
+# PCA of Rushing block
+rush_prcomp = prcomp(offense[, 1:3], scale. = TRUE)
+# select fisrt component
+rush_pc1 = rush_prcomp$x[, 1]
+# PCA of Passing block
+pass_prcomp = prcomp(offense[, 4:6], scale. = TRUE)
+# select fisrt component
+pass_pc1 = pass_prcomp$x[, 1]
+
+# compare nipals components versus prcomp components
+head(cbind(rush1, pass1, rush_pc1, pass_pc1))
+
+
+# another dataset for two-step approach
+other_twostep = cbind(offense[,c(7:8, 1:6)], rush_pc1, pass_pc1,
+                      offense[,9:11])
+# apply plspm
+other_pls2 = plspm(other_twostep, nfl_path, nfl_blocks2, modes = nfl_modes)
+
+# plot path coeffs
+plot(other_pls2)
